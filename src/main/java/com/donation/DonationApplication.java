49f7c108ccc0b2e7b216d8307c1a5c0c896856ee
Madhu696969 <2400030376@kluniversity.in>
2026-04-28 01:49:31 +0530

    first commit

--- src/main/java/com/donation/DonationApplication.java
+++ src/main/java/com/donation/DonationApplication.java
@@ -2,11 +2,15 @@
 package com.donation;
 
 import org.springframework.boot.SpringApplication;
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class DonationApplication {
     public static void main(String[] args) {
+
+		Dotenv dotenv = Dotenv.configure().ignoreIfMissing().load();
+		dotenv.entries().forEach(e -> System.setProperty(e.getKey(), e.getValue()));
         SpringApplication.run(DonationApplication.class, args);
     }
 }
--- src/main/java/com/donation/config/CorsConfig.java
+++ src/main/java/com/donation/config/CorsConfig.java
@@ -2,28 +2,25 @@ package com.donation.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.*;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class CorsConfig {
 
     @Bean
-    WebMvcConfigurer corsConfigurer() {
-
+    public WebMvcConfigurer corsConfigurer() {
         return new WebMvcConfigurer() {
 
             @Override
             public void addCorsMappings(CorsRegistry registry) {
 
                 registry.addMapping("/**")
-                        .allowedOriginPatterns("*") // important
-                        .allowedMethods(
-                                "GET",
-                                "POST",
-                                "PUT",
-                                "DELETE",
-                                "OPTIONS"
+                        .allowedOrigins(
+                                "https://fsad-project-phi.vercel.app",
+                                "http://localhost:3000" // optional for local testing
                         )
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true);
             }
